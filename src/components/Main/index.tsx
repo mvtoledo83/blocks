624522fe75b739b3ddff8809b72a624d159b2549
Marcos De Farias <mvtoledo83@gmail.com>
2023-05-31 12:24:52 -0300

    feat: media-query

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,24 +5,42 @@ import HeaderMain from "./components/HeaderMain";
 const Main = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     const getBlocksApi = async () => {
       try {
         setLoading(true);
         const response = await fetch(
-          `https://test-candidaturas-front-end.onrender.com/families?skip=0&take=20`
+          `https://test-candidaturas-front-end.onrender.com/families?skip=${
+            page * 20
+          }&take=20`
         );
         const result = await response.json();
         const artData = result;
-        setData(artData);
+        setData((prevData) => [...prevData, ...artData]);
         setLoading(false);
       } catch (error) {
         console.error("Error:", error);
       }
     };
+
     getBlocksApi();
-  }, []);
+
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      if (scrollTop + windowHeight >= documentHeight) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [page]);
 
   return (
     <main id="blocks-front-main" className="blocks-front-main">
@@ -41,57 +59,66 @@ const Main = () => {
           id="blocks-front-main-article-ul"
           className="blocks-front-main-article-ul"
         >
-          {data.map((blockItems) => (
-            <li
-              id="blocks-front-main-article-ul-li"
-              className="blocks-front-main-article-ul-li"
-              key={blockItems.id}
-            >
-              <div
-                id="blocks-front-main-article-ul-li-div-img"
-                className="blocks-front-main-article-ul-li-div-img"
-              >
-                <img
-                  id="blocks-front-main-article-ul-li-div-img-image"
-                  className="blocks-front-main-article-ul-li-div-img-image"
-                  src={`https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${blockItems.id}.jpg`}
-                  alt="o produto XYZ"
-                />
-              </div>
-
-              <div
-                id="blocks-front-main-article-ul-li-div-information"
-                className="blocks-front-main-article-ul-li-div-information"
-              >
-                <p
-                  id="blocks-front-main-article-ul-li-div-information-p"
-                  className="blocks-front-main-article-ul-li-div-information-p"
+          {loading && (
+            <>
+              <p>Carregando...</p>
+            </>
+          )}
+          {!loading && (
+            <>
+              {data.map((blockItems) => (
+                <li
+                  id="blocks-front-main-article-ul-li"
+                  className="blocks-front-main-article-ul-li"
+                  key={blockItems.id}
                 >
-                  {blockItems.details.name}
-                </p>
-                <span
-                  id="blocks-front-main-article-ul-li-div-information-span"
-                  className="blocks-front-main-article-ul-li-div-information-span"
-                >
-                  <svg
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                  <div
+                    id="blocks-front-main-article-ul-li-div-img"
+                    className="blocks-front-main-article-ul-li-div-img"
                   >
-                    <path
-                      d="M2.63868 1.72043L8.32001 1.72035M8.32001 1.72035L8.32001 7.32088M8.32001 1.72035L1.72035 8.32001"
-                      stroke="#474747"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                    <img
+                      id="blocks-front-main-article-ul-li-div-img-image"
+                      className="blocks-front-main-article-ul-li-div-img-image"
+                      src={`https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${blockItems.id}.jpg`}
+                      alt="o produto XYZ"
                     />
-                  </svg>
-                </span>
-              </div>
-            </li>
-          ))}
+                  </div>
+
+                  <div
+                    id="blocks-front-main-article-ul-li-div-information"
+                    className="blocks-front-main-article-ul-li-div-information"
+                  >
+                    <p
+                      id="blocks-front-main-article-ul-li-div-information-p"
+                      className="blocks-front-main-article-ul-li-div-information-p"
+                    >
+                      {blockItems.details.name}
+                    </p>
+                    <span
+                      id="blocks-front-main-article-ul-li-div-information-span"
+                      className="blocks-front-main-article-ul-li-div-information-span"
+                    >
+                      <svg
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M2.63868 1.72043L8.32001 1.72035M8.32001 1.72035L8.32001 7.32088M8.32001 1.72035L1.72035 8.32001"
+                          stroke="#474747"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </>
+          )}
         </ul>
       </article>
     </main>
@@ -99,4 +126,3 @@ const Main = () => {
 };
 
 export default Main;
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./styles.scss";
 import "../assets/fonts/fonts.scss";
 import Header from "../components/Header";
