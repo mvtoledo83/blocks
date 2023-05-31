a5b5127e46305ad51788f7f2747c77c8bfb3139c
Marcos De Farias <mvtoledo83@gmail.com>
2023-05-31 11:27:09 -0300

    feat: media-query

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -99,3 +99,4 @@ const Main = () => {
 };
 
 export default Main;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import "./styles.scss";
+import "../assets/fonts/fonts.scss";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import PremiumBanner from "../components/PremiumBanner";
 import PremiumBannerMobile from "../components/PremiumBannerMobile";
+
+
 
 const Index = () => {
   return (
